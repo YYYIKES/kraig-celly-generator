75887bfe09e786958688da3b782d12c44e26af44
YYYIKES! <contact@ashcrook.com>
2021-09-07 20:55:00 +1000

    edit state size etc

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ with open('static/datasets/reworked.txt') as f:
     text = f.read()
 
 text_model = markovify.Text(
-    text, state_size=3, well_formed=False)
+    text, state_size=2, well_formed=False)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        for i in range(1):
+        for i in range(5):
             genText = text_model.make_short_sentence(
-                10000, min_chars=30, tries=1000, max_overlap_ratio=0.5)
+                10000, min_chars=400, tries=500, max_overlap_ratio=0.5)
         return render_template('index.html', genText=genText)
     else:
         return render_template('index.html')
